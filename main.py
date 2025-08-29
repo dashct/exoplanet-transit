55867d770b9d83b3f426ea6a5ae7cad565be29f8
honk <dylanct0808@gmail.com>
2025-08-29 22:35:53 +0100

    period and radius

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from lightkurve import search_targetpixelfile
+import lightkurve as lk
+import numpy as np
+import matplotlib.pyplot as plt
 
-tpf = search_targetpixelfile('Kepler-10', author='Kepler', cadence='long', quarter=4).download()
-lc = tpf.to_lightcurve(aperture_mask=tpf.pipeline_mask)
+PERIOD = np.linspace(0.5, 1, 10000)
+R_star = 1.065
 
-lc.plot()
+search_result = lk.search_lightcurve('Kepler-10', author='Kepler', cadence='long').download_all()
+lc = search_result.stitch().flatten().remove_outliers()
 
-adjusted_lc = lc.flatten()
-adjusted_lc.plot()
+bls = lc.to_periodogram(method='bls', period=PERIOD, frequency_factor=500)
+
+planet_period = bls.period_at_max_power
+planet_t0 = bls.transit_time_at_max_power
+
+lc_folded = lc.fold(period=planet_period, epoch_time=planet_t0).bin(0.001).scatter()
+
+lc_folded.plot()
+lower_line = 1 - 0.00018
+upper_line = 1 + 0.00001
+plt.axhline(lower_line) # Bottom
+plt.axhline(upper_line) # Top
+depth = upper_line - lower_line
+
+R_planet = 109 * R_star * np.sqrt(depth)
